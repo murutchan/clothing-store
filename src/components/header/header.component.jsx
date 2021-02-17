18562d7134a9d1c7bfeec1fe5044491d0c5f6e4f
Askar Talantbek <murutchan@gmail.com>
2021-02-17 16:04:54 -0600

    shopping cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -27,6 +28,7 @@ const Header = ({ currentUser }) => (
           SIGN IN{" "}
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
